dea157313ed1cbeda48e307ee7df1a8759f7968c
ivy <ivory@combinate.me>
2023-04-16 20:32:27 +0800

    [TW#21311969] - Registration and Sign-in pages

---
+++ modules/client/public/assets/services/apiServices.js
@@ -0,0 +1,54 @@
+var apiServices = (function () {
+    const
+        apiUrl = "/api",
+        request = axios.create({});
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('[name="csrf-token"]').content
+        }
+    };
+
+    // Axios Processor
+    async function processRequest(method, url, payload, headers = config.headers) {
+        let endpoint = apiUrl + url;
+        let process = method.toLowerCase() === 'get'
+            ? request.get(endpoint, { headers })
+            : request[method](endpoint, payload, { headers });
+
+        return await process.then(response => {
+            if (response.data.errors) {
+                if (typeof response.data.errors === "object" && !Object.keys(response.data.errors).length) {
+                    return { state: true, data: response.data };
+                } else {
+                    console.error(`API Error: ${url}`, response.data.errors);
+                    return { state: false, data: response.data };
+                }
+            }
+            return { state: true, data: response.data };
+        }).catch(error => {
+            if (error.response.data && error.response.data.errors) {
+                console.error(`API Error: ${url}`, error.response.data.errors);
+            }
+            return { state: false, data: error.response.data };
+        });
+    }
+
+    return {
+        updateCartItem: async (payload) => {
+            let url = `/update-cart-api.json`;
+            return await processRequest('post', url, payload);
+        },
+        validateDiscountCode: async (payload) => {
+            let url = `/validate-discount-code.json`;
+            return await processRequest('post', url, payload);
+        },
+        removeDiscountCode: async (id) => {
+            let url = `/remove-discount-code.json`;
+            return await processRequest('post', url, id);
+        },
+        processRequest: processRequest
+    }
+})();
